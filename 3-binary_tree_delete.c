e7c0a90314141ac6547c077aef1a01b62f8cb551
new-name <dligui.ghizlane@gmail.com>
2023-10-04 10:05:05 -0700

    m

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,4 +1,4 @@
-nclude "binary_trees.h"
+include "binary_trees.h"
 
 /**
  *  * binary_tree_delete - Deletes a binary tree.
@@ -6,10 +6,11 @@ nclude "binary_trees.h"
  *    */
 void binary_tree_delete(binary_tree_t *tree)
 {
-		if (tree != NULL)
-				{
-							binary_tree_delete(tree->left);
-									binary_tree_delete(tree->right);
-											free(tree);
-												}
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);			
+		free(tree);
+
+	}
 }
